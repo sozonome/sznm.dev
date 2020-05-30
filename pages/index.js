384a8baa0f44a92bfed013f97739a52eb20cee2e
sozonome <berserk.nate@gmail.com>
2020-05-31 02:06:11 +0700

    Update add

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
         <ProjectCard title="KapturaLumina" img="/icon_kapturalumina.png" link="https://agustinusnathaniel.com/kapturalumina" />
         <ProjectCard title="COVID-19 Data" img="/icon-covid-19-data.png" link="https://covid19.sznm.dev" />
         <ProjectCard title="Le Cook" img="/icon_lecook.png" link="https://le-cook.sznm.dev" />
+        <ProjectCard title="Other Projects" link='https://agustinusnathaniel.com'/> 
       </main>
     </Layout>
   );
