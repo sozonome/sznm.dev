0d306fc119533324176e600fe233875fc51a531e
sozonome <berserk.nate@gmail.com>
2020-06-03 23:08:52 +0700

    ✨#BlackLivesMatter

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <main>
+        <p style={{marginBottom:'20px', textAlign:'center', textDecoration:'underline'}}>#BlackLivesMatter</p>
         <ProjectCard title='Personal Site' img='/avataaars.svg' link="https://agustinusnathaniel.com" />
         <ProjectCard title="KapturaLumina" img="/icon_kapturalumina.png" link="https://agustinusnathaniel.com/kapturalumina" />
         <ProjectCard title="COVID-19 Data" img="/icon-covid-19-data.png" link="https://covid19.sznm.dev" />
