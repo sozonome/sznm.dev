ca99b552419277f5b1dc54e5494b6bfbea1297e0
sozonome <berserk.nate@gmail.com>
2020-06-12 20:43:16 +0700

    Styling

--- pages/kapturaluminaprivacyterms.js
+++ pages/kapturaluminaprivacyterms.js
@@ -5,49 +5,49 @@ export default function kapturaluminaprivacyterms() {
   return (
     <Layout>
       <article>
-        <h1>KapturaLumina</h1>
-        <h2>Privacy Policy</h2>
-        <p>
+        <h1 className="text-3xl font-bold">KapturaLumina</h1>
+        <h2 className="text-1xl font-bold">Privacy Policy</h2>
+        <p className="my-2">
           Your privacy is important to us. It is sznm's policy to respect your
           privacy regarding any information we may collect from you through our
           app, KapturaLumina.
         </p>
-        <p>
+        <p className="my-2">
           We only ask for personal information when we truly need it to provide
           a service to you. We collect it by fair and lawful means, with your
           knowledge and consent. We also let you know why we’re collecting it
           and how it will be used.
         </p>
-        <p>
+        <p className="my-2">
           We only retain collected information for as long as necessary to
           provide you with your requested service. What data we store, we’ll
           protect within commercially acceptable means to prevent loss and
           theft, as well as unauthorized access, disclosure, copying, use or
           modification.
         </p>
-        <p>
+        <p className="my-2">
           We don’t share any personally identifying information publicly or with
           third-parties, except when required to by law.
         </p>
-        <p>
+        <p className="my-2">
           Our app may link to external sites that are not operated by us. Please
           be aware that we have no control over the content and practices of
           these sites, and cannot accept responsibility or liability for their
           respective privacy policies.
         </p>
-        <p>
+        <p className="my-2">
           You are free to refuse our request for your personal information, with
           the understanding that we may be unable to provide you with some of
           your desired services.
         </p>
-        <p>
+        <p className="my-2">
           Your continued use of our app will be regarded as acceptance of our
           practices around privacy and personal information. If you have any
           questions about how we handle user data and personal information, feel
           free to contact us.
         </p>
-        <p>This policy is effective as of 29 May 2020.</p>
-        <p>
+        <p className="my-2">This policy is effective as of 29 May 2020.</p>
+        <p className="my-2">
           <a href='https://getterms.io' title='Generate a free privacy policy'>
             Privacy Policy created with GetTerms.
           </a>
