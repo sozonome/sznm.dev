b1ece36638c737c6dcd11e0e0539a7f802f6607d
sozonome <berserk.nate@gmail.com>
2020-07-04 15:46:35 +0700

    📖Add Projects KapturaLumina

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,57 @@ import ProjectCard from '../components/ProjectCard';
 import { links } from '../data/links';
 
 export default function Home() {
+  const projects = [
+    {
+      title: 'Personal Site',
+      img: '/avataaars.svg',
+      link: 'https://agustinusnathaniel.com',
+    },
+    {
+      title: 'KapturaLumina',
+      img: '/app_icons/icon_kapturalumina.png',
+      link:
+        '/kapturalumina',
+    },
+    {
+      title: 'COVID-19 Data',
+      img: '/app_icons/icon-covid-19-data.png',
+      link: 'https://covid19.sznm.dev',
+    },
+    {
+      title: 'Le Cook',
+      img: '/app_icons/icon_lecook.png',
+      link: 'https://le-cook.sznm.dev',
+    },
+    {
+      title: 'RagamBudaya',
+      img: '/app_icons/ragambudaya-logo.png',
+      link: 'https://play.google.com/store/apps/details?id=app.hatchoko.ragambudaya'
+    },
+    {
+      title: 'Advice Generator',
+      img: '/app_icons/icon_advicegen.png',
+      link: 'https://advicegen.sznm.dev',
+    },
+    {
+      title: 'Design Bookmarks',
+      img: '/app_icons/desbook-pwa.png',
+      link: 'https://desbook-pwa.sznm.dev'
+    },
+    {
+      title: 'Insta Profile',
+      img: '/app_icons/icon_instaprofile.png',
+      link: 'https://instaprofile.sznm.dev',
+    },
+    {
+      title: 'Other Projects',
+      link: 'https://agustinusnathaniel.com/projects',
+    },
+    {
+      title: 'bookmarks',
+      link: 'https://sozonome.github.io/bookmarks',
+    }
+  ];
   return (
     <Layout>
       <main className='wrapper'>
@@ -12,69 +63,44 @@ export default function Home() {
             target='_blank'
             rel='noreferrer noopener'
           >
-            <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
+            <img src='https://img.icons8.com/fluent/48/000000/instagram-new.png' />
           </a>
           <a
             href={`https://github.com/${links.github}`}
             target='_blank'
             rel='noreferrer noopener'
           >
-            <img src="https://img.icons8.com/fluent/48/000000/repository.png"/>
+            <img src='https://img.icons8.com/fluent/48/000000/repository.png' />
           </a>
           <a
             href={`https://twitter.com/${links.twitter}`}
             target='_blank'
             rel='noreferrer noopener'
           >
-            <img src="https://img.icons8.com/fluent/48/000000/twitter.png"/>
+            <img src='https://img.icons8.com/fluent/48/000000/twitter.png' />
           </a>
           <a
             href={`https://youtube.com/channel/${links.youtube}`}
             target='_blank'
             rel='noreferrer noopener'
           >
-            <img src="https://img.icons8.com/fluent/48/000000/youtube-play.png"/>
+            <img src='https://img.icons8.com/fluent/48/000000/youtube-play.png' />
           </a>
         </div>
         <div className='flex flex-wrap pt-4'>
-          <ProjectCard
-            title='Personal Site'
-            img='/avataaars.svg'
-            link='https://agustinusnathaniel.com'
-          />
-          <ProjectCard
-            title='KapturaLumina'
-            img='/app_icons/icon_kapturalumina.png'
-            link='https://play.google.com/store/apps/details?id=dev.sznm.kapturalumina'
-          />
-          <ProjectCard
-            title='COVID-19 Data'
-            img='/app_icons/icon-covid-19-data.png'
-            link='https://covid19.sznm.dev'
-          />
-          <ProjectCard
-            title='Le Cook'
-            img='/app_icons/icon_lecook.png'
-            link='https://le-cook.sznm.dev'
-          />
-          <ProjectCard
-            title='Advice Generator'
-            img='/app_icons/icon_advicegen.png'
-            link='https://advicegen.sznm.dev'
-          />
-          <ProjectCard
-            title='Insta Profile'
-            img='/app_icons/icon_instaprofile.png'
-            link='https://instaprofile.sznm.dev'
-          />
-          <ProjectCard
-            title='Other Projects'
-            link='https://agustinusnathaniel.com/projects'
-          />
-          <ProjectCard
-            title='bookmarks'
-            link='https://sozonome.github.io/bookmarks'
-          />
+          {projects.map((project) => {
+            if (project.img) {
+              return (
+                <ProjectCard
+                  title={project.title}
+                  img={project.img}
+                  link={project.link}
+                />
+              );
+            } else {
+              return <ProjectCard title={project.title} link={project.link} />;
+            }
+          })}
         </div>
         <div className='sm:flex'>
           <img
---
+++ pages/kapturalumina.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function kapturaluminaprivacyterms() {
+  return (
+    <Layout>
+      <article>
+        <img className="w-1/4" src="/app_icons/icon_kapturalumina.png" alt=""/>
+        <h1 className="text-3xl font-bold">KapturaLumina</h1>
+        <h2 className="text-1xl font-bold">Basic Photography Learning App</h2>
+        <p className="my-4">
+          This is the app I build as part of my bachelor thesis project with gamification implementation.
+          The App currently only available in Indonesian language.
+        </p>
+        <div className="buttons">
+          <a target="_blank" rel="noopener noreferrer" href="https://kapturalumina.sznm.dev" className="bg-orange-800 text-orange-100 p-2 rounded-large font-bold hover:bg-orange-400">
+            Web App
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=dev.sznm.kapturalumina" className="ml-4 border border-white bg-black text-orange-100 p-2 rounded-large font-bold hover:bg-orange-400">
+            Google Play <span className="font-normal text-xs">(Android)</span>
+          </a>
+        </div>
+      </article>
+    </Layout>
+  );
+}
