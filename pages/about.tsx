a45f4445109fc3388114245d7d8e5bbd86e4e0f6
sozonome <berserk.nate@gmail.com>
2020-08-29 03:29:15 +0700

    refactor: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,47 @@
  */
 
 import Layout from "../components/layout";
+import { Box, Heading, Text, Flex, Link } from "@chakra-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const About = () => {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <Box>
+        <Heading>sozonome</Heading>
+        <Text>1.01 ^365 = 37.7</Text>
+        <Flex fontSize="4xl">
+          <Link
+            href={`https://github.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </Link>
+          <Link
+            href={`https://twitter.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </Link>
+          <Link
+            href={`https://www.instagram.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </Link>
+          <Link
+            href={`https://youtube.com/channel/UCJnYMGIHtQ8yInuq4Pc2Ttg`}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "youtube"]} />
+          </Link>
+        </Flex>
+      </Box>
+    </Layout>
+  );
 };
 
 export default About;
