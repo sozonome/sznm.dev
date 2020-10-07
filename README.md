00cb569a66405184d130af23c695a2c596219289
sozonome <berserk.nate@gmail.com>
2020-10-07 21:59:33 +0700

    content: add about content + update README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ ___
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=sozonome_sznm.dev&metric=alert_status)](https://sonarcloud.io/dashboard?id=sozonome_sznm.dev) [![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=sozonome_sznm.dev&metric=sqale_rating)](https://sonarcloud.io/dashboard?id=sozonome_sznm.dev) [![Bugs](https://sonarcloud.io/api/project_badges/measure?project=sozonome_sznm.dev&metric=bugs)](https://sonarcloud.io/dashboard?id=sozonome_sznm.dev) [![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=sozonome_sznm.dev&metric=code_smells)](https://sonarcloud.io/dashboard?id=sozonome_sznm.dev) [![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=sozonome_sznm.dev&metric=duplicated_lines_density)](https://sonarcloud.io/dashboard?id=sozonome_sznm.dev)
 
 ---
+sznm.dev is my personal dev site built using Next.js, Chakra-UI, and TypeScript.
 
 ### References 
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,43 +1,159 @@
-/**
- * @todo
- * [ ] add about content
- */
-
-import { Heading, Text, Flex, Link } from "@chakra-ui/core";
+import { Heading, Text, Flex, Link, Box, BoxProps } from "@chakra-ui/core";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import AccessibleLink from "../components/AccessibleLink";
+import MotionBox from "../components/motion/MotionBox";
+import MotionFlex from "../components/motion/MotionFlex";
+
 const About = () => {
   return (
-    <>
+    <Box>
+      <HeadSection />
+      <AboutContent />
+      <Links />
+    </Box>
+  );
+};
+
+const HeadSection = () => {
+  return (
+    <Box marginBottom={8}>
       <Heading>sozonome</Heading>
-      <Text>1.01 ^365 = 37.7</Text>
-      <Flex fontSize="4xl">
-        <Link href={`https://github.com/sozonome`} marginRight={11} isExternal>
-          <FontAwesomeIcon icon={["fab", "github"]} />
-        </Link>
-        <Link href={`https://twitter.com/sozonome`} marginRight={11} isExternal>
-          <FontAwesomeIcon icon={["fab", "twitter"]} />
-        </Link>
-        <Link
-          href={`https://www.instagram.com/sozonome`}
-          marginRight={11}
+    </Box>
+  );
+};
+
+const AboutContent = () => {
+  const textProps: BoxProps = {
+    marginY: 4,
+  };
+
+  return (
+    <MotionBox
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Heading>👋 Hi, how are you?</Heading>
+      <Text {...textProps}>
+        I'm currently working as a Frontend Engineer. Most of the time I work
+        with TypeScript and React. I make fun projects in my free time and share
+        it here. Whether to explore more around React and its frameworks
+        (Gatsby, Next.js), Ionic, or trying out Svelte and Vue.
+      </Text>
+      <Text {...textProps}>
+        I like exploring useful and fun tech products (surfing around
+        ProductHunt), traveling, and{" "}
+        <AccessibleLink
+          href="https://instagram.com/agustinusnathaniel"
           isExternal
         >
-          <FontAwesomeIcon icon={["fab", "instagram"]} />
-        </Link>
-        <Link
-          href={`https://youtube.com/channel/UCJnYMGIHtQ8yInuq4Pc2Ttg`}
-          marginRight={11}
-          isExternal
-        >
-          <FontAwesomeIcon icon={["fab", "youtube"]} />
-        </Link>
-        <Link href={`mailto:hello@sznm.dev`} isExternal>
-          <FontAwesomeIcon icon={faEnvelope} />
-        </Link>
-      </Flex>
-    </>
+          <Text
+            as="span"
+            fontStyle="italic"
+            textDecoration="underline"
+            fontWeight={500}
+          >
+            doing some visual arts (graphic design, videography, photography).
+          </Text>
+        </AccessibleLink>
+      </Text>
+    </MotionBox>
+  );
+};
+
+const Links = () => {
+  const wrapperAnimationProps = {
+    variants: {
+      before: { y: 0, opacity: 0, transition: { type: "spring" } },
+      after: { y: 20, opacity: 1, transition: { type: "spring" } },
+    },
+    initial: "before",
+    animate: "after",
+  };
+
+  const childAnimationProps = {
+    variants: {
+      before: {
+        opacity: 0,
+        y: 20,
+        transition: {
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+        },
+      },
+      after: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+        },
+      },
+    },
+  };
+
+  return (
+    <MotionBox marginY={4} {...wrapperAnimationProps}>
+      <Text fontStyle="italic">1.01 ^365 = 37.7</Text>
+      <MotionFlex
+        fontSize="4xl"
+        variants={{
+          before: {},
+          after: { transition: { staggerChildren: 0.06 } },
+        }}
+        initial="before"
+        animate="after"
+      >
+        <MotionBox {...childAnimationProps}>
+          <Link
+            href={`https://github.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </Link>
+        </MotionBox>
+        <MotionBox {...childAnimationProps}>
+          <Link
+            href={`https://twitter.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </Link>
+        </MotionBox>
+        <MotionBox {...childAnimationProps}>
+          <Link
+            href={`https://www.instagram.com/sozonome`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </Link>
+        </MotionBox>
+        <MotionBox {...childAnimationProps}>
+          <Link
+            href={`https://youtube.com/channel/UCJnYMGIHtQ8yInuq4Pc2Ttg`}
+            marginRight={11}
+            isExternal
+          >
+            <FontAwesomeIcon icon={["fab", "youtube"]} />
+          </Link>
+        </MotionBox>
+        <MotionBox {...childAnimationProps}>
+          <Link href={`mailto:hello@sznm.dev`} isExternal>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
+        </MotionBox>
+      </MotionFlex>
+    </MotionBox>
   );
 };
 
