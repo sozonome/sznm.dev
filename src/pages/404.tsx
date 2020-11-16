87922d22f95bbe42628e0c59b73036ac4c0bd695
Agustinus Nathaniel <berserk.nate@gmail.com>
2020-11-16 23:52:32 +0700

    feat(pages): add custom 404 page (#10)

---
+++ src/pages/404.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Link as ChakraLink,
+  useColorMode,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+import MotionBox from "../components/motion/MotionBox";
+
+const Page404 = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <MotionBox
+        width={["100%", "70%", "60%", "60%"]}
+        margin="0 auto"
+        animate={{ y: 20 }}
+        transition={{ repeat: Infinity, duration: 2, repeatType: "reverse" }}
+      >
+        <Image unsized src="/404 Error-rafiki.svg" />
+      </MotionBox>
+      <Text textAlign="center" fontSize="xs">
+        <ChakraLink href="https://stories.freepik.com/web" isExternal>
+          Illustration by Freepik Stories
+        </ChakraLink>
+      </Text>
+
+      <Box marginY={4}>
+        <Heading textAlign="center">Seems like you're lost.</Heading>
+
+        <Box textAlign="center">
+          <Link href="/" passHref>
+            <Button
+              backgroundColor={colorMode === "light" ? "gray.300" : "teal.500"}
+            >
+              Let's Head Back
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Page404;
