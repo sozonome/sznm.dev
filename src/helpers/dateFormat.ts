aee768e4aa4b7e4aad1479719738836f39785dc1
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-03-26 10:03:14 +0700

    fix build error

--- src/helpers/dateFormat.ts
+++ src/helpers/dateFormat.ts
@@ -9,7 +9,7 @@ export const dateFormatLong = (date: string, time: boolean = false) => {
     minute: "numeric",
   };
 
-  const optionsWithoutTime = {
+  const optionsWithoutTime: Intl.DateTimeFormatOptions = {
     day: "numeric",
     month: "long",
     year: "numeric",
