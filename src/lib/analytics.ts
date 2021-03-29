d9881b91f28b1b0a1552ac3681aa115bedd733b3
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-03-30 00:58:47 +0700

    remove console.log

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,12 +1,10 @@
 import ReactGA from 'react-ga'
 
 export const initGA = () => {
-  console.log('GA init')
   ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID)
 }
 
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`)
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
