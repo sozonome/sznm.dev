842f483cb3fa120eacee5ec3ec0ce35cd8f83514
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-03-30 00:56:43 +0700

    feat(analytics): implement google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,38 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import "@fontsource/recursive/latin.css";
 import "@fontsource/catamaran/latin.css";
 
 import Layout from "../components/layout";
 
+import { initGA, logPageView } from "../lib/analytics";
+
 import customTheme from "../styles/customTheme";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    initGA();
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes("?")) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={customTheme}>
       <Layout>
