a6b6453332f9b8ec132a1dba91d43beaeac34160
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-08-29 12:55:25 +0700

    fix(theme): :bug: fix typo

--- src/styles/theme/components/button.ts
+++ src/styles/theme/components/button.ts
@@ -1,6 +1,6 @@
 import { DeepPartial, Theme } from "@chakra-ui/react";
 
-const Button: DeepPartial<Theme["components"]["C"]> = {
+const Button: DeepPartial<Theme["components"]["Button"]> = {
   baseStyle: {
     borderRadius: "1rem",
   },
