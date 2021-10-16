0395f46c210435ad38d1e71d28a363484f889fbf
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-16 16:11:40 +0700

    feat(layout): :speech_balloon: add additional links in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,55 @@
-import { Box, Link, Text } from "@chakra-ui/react";
+import { Box, Grid, Link, Stack, Text } from "@chakra-ui/react";
+
+type FooterLinkProps = {
+  label: string;
+  url: string;
+};
+
+const links: Array<FooterLinkProps> = [
+  {
+    label: "Base",
+    url: "https://base.sznm.dev",
+  },
+  {
+    label: "Templates",
+    url: "https://github.com/sozonome?tab=repositories&q=template,boilerplate",
+  },
+  {
+    label: "Timeline",
+    url: "https://poly.sznm.dev",
+  },
+  {
+    label: "Misc",
+    url: "https://my-space.sznm.dev",
+  },
+];
+
+const FooterLink = ({ label, url }: FooterLinkProps) => {
+  return (
+    <Box>
+      <Link href={url} isExternal fontWeight="semibold" fontFamily="heading">
+        {label}
+      </Link>
+    </Box>
+  );
+};
 
 const Footer = () => {
   return (
-    <Box as="footer" width="full" layerStyle="layoutBlock">
+    <Stack as="footer" width="full" layerStyle="layoutBlock" spacing={6}>
+      <Grid
+        display={{ base: "grid", md: "flex" }}
+        templateColumns={{
+          base: "repeat(2, 1fr)",
+          md: `repeat(${links.length <= 4 ? links.length : 4}, 1fr)`,
+        }}
+        gap={{ base: 4, md: 6 }}
+      >
+        {links.map((link) => (
+          <FooterLink {...link} />
+        ))}
+      </Grid>
+
       <Text fontSize={["xs", "sm"]}>
         2020 - {new Date().getFullYear()} |{" "}
         <Link
@@ -13,7 +60,7 @@ const Footer = () => {
           AgustinusNathaniel
         </Link>
       </Text>
-    </Box>
+    </Stack>
   );
 };
 
