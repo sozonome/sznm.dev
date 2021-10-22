240189eab8c01f26475aee56499b5da17781b2c5
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-22 13:06:14 +0700

    chore: use preact for production build

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ module.exports = {
     nftTracing: true,
   },
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
