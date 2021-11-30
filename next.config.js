3925830ed20e6d8704a675cd9d30f86bf245a14f
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-11-30 19:00:50 +0700

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
       });
     }
 
     return config;
   },
-};
+});
