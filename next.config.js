f3aa15b2eb12d45d05f8b602be757a72cacb7c89
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-12-13 19:41:15 +0700

    feat: :lock: add security headers

--- next.config.js
+++ next.config.js
@@ -2,10 +2,79 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+/**
+ * @docs
+ * - https://scotthelme.co.uk/content-security-policy-an-introduction/
+ * - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+ */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' umami.sznm.dev;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data: *.freepik.com;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+/**
+ * @docs
+ * - https://nextjs.org/docs/advanced-features/security-headers
+ */
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=()",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: "/",
+        headers: securityHeaders,
+      },
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
