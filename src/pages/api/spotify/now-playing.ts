f2675122030c124d090cc92d4c51490731e8d42f
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-02-11 22:01:52 +0700

    fix(services): 🐛 add error handling in spotify api route (#74)

--- src/pages/api/spotify/now-playing.ts
+++ src/pages/api/spotify/now-playing.ts
@@ -8,22 +8,26 @@ const nowPlaying = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400);
   }
 
-  const response = await getNowPlaying();
+  try {
+    const response = await getNowPlaying();
 
-  if (!response || !response.item) {
+    if (!response || !response.item) {
+      return res.status(200).json({ isPlaying: false });
+    }
+
+    const data: GetNowPlayingTransformed = {
+      isPlaying: response.is_playing,
+      trackTitle: response.item.name,
+      artist: response.item.album.artists.map(({ name }) => name).join(", "),
+      album: response.item.album.name,
+      albumArtUrl: response.item.album.images[0].url,
+      trackUrl: response.item.external_urls.spotify,
+    };
+
+    return res.status(200).json(data);
+  } catch {
     return res.status(200).json({ isPlaying: false });
   }
-
-  const data: GetNowPlayingTransformed = {
-    isPlaying: response.is_playing,
-    trackTitle: response.item.name,
-    artist: response.item.album.artists.map(({ name }) => name).join(", "),
-    album: response.item.album.name,
-    albumArtUrl: response.item.album.images[0].url,
-    trackUrl: response.item.external_urls.spotify,
-  };
-
-  return res.status(200).json(data);
 };
 
 export default nowPlaying;
