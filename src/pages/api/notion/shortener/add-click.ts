f8fcfb693299b248528fbc2d0eab21ac41d8e327
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-16 01:50:17 +0700

    refactor: revise add click api route implementation

--- src/pages/api/notion/shortener/add-click.ts
+++ src/pages/api/notion/shortener/add-click.ts
@@ -5,12 +5,15 @@ import type { ShortenedUrlEntry } from "lib/services/notion/link-shortener/types
 
 const addClick = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
-    return res.status(400);
+    return res.status(405).json({ message: "Unallowed Method" });
+  }
+  if (!req.body) {
+    return res.status(400).json({ message: "Bad Request" });
   }
 
   const entry = req.body as ShortenedUrlEntry;
   await addUrlClick(entry);
-  return res.status(200);
+  return res.status(200).send("OK");
 };
 
 export default addClick;
