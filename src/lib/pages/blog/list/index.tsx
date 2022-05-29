44e37c6a74eb7e5f3735f48b908f9f7c13ff8a25
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-29 17:00:35 +0700

    feat(blog): ✨ add search box

--- src/lib/pages/blog/list/index.tsx
+++ src/lib/pages/blog/list/index.tsx
@@ -1,5 +1,16 @@
-import { Heading, Box, Text, Grid } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Text,
+  Grid,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import debounce from "lodash-es/debounce";
 import { NextSeo } from "next-seo";
+import * as React from "react";
+import { FaSearch } from "react-icons/fa";
 
 import BlogPostPreview from "lib/components/blog/BlogPostPreview";
 import { baseUrl } from "lib/constants/baseUrl";
@@ -8,9 +19,19 @@ import { sznmOgImage } from "lib/utils/sznmOgImage";
 import type { BlogPostListProps } from "./types";
 
 const BlogPostList = ({ allPostsData }: BlogPostListProps) => {
-  const blogPosts = allPostsData.map((postData) => (
-    <BlogPostPreview postData={postData} key={postData.title} />
-  ));
+  const [keyword, setKeyword] = React.useState<string>("");
+
+  const filteredPosts = allPostsData.filter((post) =>
+    post.title.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  const handleChangeKeyword = React.useCallback(
+    () =>
+      debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyword(event.target.value);
+      }, 200),
+    []
+  );
 
   return (
     <Box>
@@ -28,15 +49,31 @@ const BlogPostList = ({ allPostsData }: BlogPostListProps) => {
         }}
       />
 
-      <Box marginBottom={22}>
-        <Heading as="h1" size="xl" marginBottom={2}>
+      <Grid marginBottom={22} gap={2}>
+        <Heading as="h1" size="xl">
           Blog Posts
         </Heading>
         <Text>Just some writings</Text>
-      </Box>
+      </Grid>
+
+      <InputGroup>
+        <Input
+          variant="filled"
+          type="text"
+          defaultValue={keyword}
+          onChange={handleChangeKeyword()}
+          placeholder="Search posts"
+        />
+        <InputRightElement>
+          <FaSearch />
+        </InputRightElement>
+      </InputGroup>
 
       <Grid gap={16} marginY={12}>
-        {blogPosts}
+        {!filteredPosts.length && <Text>No posts found.</Text>}
+        {filteredPosts.map((postData) => (
+          <BlogPostPreview postData={postData} key={postData.title} />
+        ))}
       </Grid>
     </Box>
   );
