fcfde4fe51387a186c9c5ca16d66a2eb1ecb1941
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-07-23 00:28:26 +0700

    refactor(snippets): add meta to snippet list

--- src/lib/pages/snippets/list/index.tsx
+++ src/lib/pages/snippets/list/index.tsx
@@ -1,4 +1,5 @@
 import { Grid, Heading, Text } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
 
 import MotionBox from "lib/components/motion/MotionBox";
 import MotionGrid from "lib/components/motion/MotionGrid";
@@ -7,12 +8,31 @@ import {
   childAnimationProps,
   staggerAnimationProps,
 } from "lib/constants/animation";
+import { baseUrl } from "lib/constants/baseUrl";
+import { sznmOgImage } from "lib/utils/sznmOgImage";
 
 import type { SnippetListProps } from "./types";
 
 const SnippetList = ({ snippets }: SnippetListProps) => {
   return (
     <Grid gap={12}>
+      <NextSeo
+        title="Snippets"
+        canonical={`${baseUrl}/snippets`}
+        openGraph={{
+          title: "Snippets | sozonome",
+          images: [
+            {
+              url: sznmOgImage({
+                heading: "Snippets | sozonome",
+                text: "https://sznm.dev",
+              }),
+              alt: "Snippets | sozonome og-image",
+            },
+          ],
+        }}
+      />
+
       <Grid gap={2}>
         <Heading>Snippets</Heading>
         <Text color="gray.500">
