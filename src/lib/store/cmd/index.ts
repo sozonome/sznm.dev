d0afd5fdccf626b0a08fd83abce104f5942bba2a
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-08-14 11:32:05 +0700

    refactor(cmd): optimize store and hook

--- src/lib/store/cmd/index.ts
+++ src/lib/store/cmd/index.ts
@@ -4,11 +4,6 @@ import type { CommandMenuStore } from "./types";
 
 export const useCmdMenu = create<CommandMenuStore>((set) => ({
   isOpen: false,
-  openCmdMenu: () => {},
-  closeCmdMenu: () => {},
-  onSelectItem: () => {},
-  setIsOpen: (isOpen) => set({ isOpen }),
-  setOpenCmdMenu: (openCmdMenu) => set({ openCmdMenu }),
-  setCloseCmdMenu: (closeCmdMenu) => set({ closeCmdMenu }),
-  setOnSelectItem: (onSelectItem) => set({ onSelectItem }),
+  openCmdMenu: () => set({ isOpen: true }),
+  closeCmdMenu: () => set({ isOpen: false }),
 }));
