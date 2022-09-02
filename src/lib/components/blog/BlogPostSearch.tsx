858b2b3755c42506438b44e48f7372857cd4e557
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-09-02 22:11:06 +0700

    refactor(layout): dynamic import layout components (#123)
    reduce almost 30% first load JS

---
+++ src/lib/components/blog/BlogPostSearch.tsx
@@ -0,0 +1,26 @@
+import { InputGroup, Input, InputRightElement } from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+
+type BlogPostSearchProps = {
+  keyword: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+const BlogPostSearch = ({ keyword, onChange }: BlogPostSearchProps) => {
+  return (
+    <InputGroup>
+      <Input
+        variant="filled"
+        type="text"
+        defaultValue={keyword}
+        onChange={onChange}
+        placeholder="Search posts"
+      />
+      <InputRightElement>
+        <FaSearch />
+      </InputRightElement>
+    </InputGroup>
+  );
+};
+
+export default BlogPostSearch;
