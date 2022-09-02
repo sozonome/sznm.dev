8e05e4192f52bb2d4e890747ec05671765dc5316
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-09-02 23:25:06 +0700

    refactor(services): replace embed shortener with external shortener (#124)

--- src/lib/services/notion/link-shortener/addUrlClick.ts
+++
@@ -1,6 +0,0 @@
-import { updatePage } from "lib/services/notion/common/page";
-
-import type { ShortenedUrlEntry } from "./types";
-
-export const addUrlClick = (urlEntry: ShortenedUrlEntry) =>
-  updatePage(urlEntry.id, { clicks: { number: urlEntry.clicks + 1 } });
