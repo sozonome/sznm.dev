084f71d795ab0faaf1af2332fa740f9d195281f8
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-09-02 21:31:39 +0700

    chore: update packages to latest

--- src/lib/styles/theme/components/index.ts
+++ src/lib/styles/theme/components/index.ts
@@ -1,10 +1,8 @@
-import type { DeepPartial, Theme } from "@chakra-ui/react";
-
 import { Button } from "./button";
 import { Code } from "./code";
 import { Heading } from "./heading";
 
-export const components: DeepPartial<Theme["components"]> = {
+export const components = {
   Button,
   Code,
   Heading,
