cd1341eb9957c6054f06a7de417eaf11437d9a02
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-12-30 17:27:40 +0700

    chore: setup tailwindcss (#166)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Recursive", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
