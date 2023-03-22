c6546b47ee072a3ed09db0365361a995578ad0e6
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-03-22 15:33:26 +0700

    refactor: move api route file

---
+++ src/lib/pages/api/now-playing/index.ts
@@ -0,0 +1,36 @@
+import type { NextRequest } from 'next/server';
+
+import { defaultHeader } from '~/lib/constants/api/header';
+import { getNowPlaying } from '~/lib/services/spotify/user/now-playing';
+
+const nowPlaying = async (req: NextRequest) => {
+  if (req.method !== 'GET') {
+    return new Response(undefined, { status: 400 });
+  }
+
+  try {
+    const response = await getNowPlaying();
+
+    if (!response || !response.isPlaying) {
+      return new Response(JSON.stringify({ isPlaying: false }), {
+        status: 200,
+        headers: defaultHeader,
+      });
+    }
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        ...defaultHeader,
+        'cache-control': 's-maxage=1, stale-while-revalidate=10',
+      },
+    });
+  } catch {
+    return new Response(JSON.stringify({ isPlaying: false }), {
+      status: 200,
+      headers: defaultHeader,
+    });
+  }
+};
+
+export default nowPlaying;
--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,28 +1,7 @@
-import { defaultHeader } from '~/lib/constants/api/header';
-import { sznmAppsProjects } from '~/lib/constants/project';
+import projects from '~/lib/pages/api/projects';
 
 export const config = {
   runtime: 'edge',
-};
-
-const projects = async () => {
-  const projectList = sznmAppsProjects.map(
-    ({ title, description, projectLink, icon }) => ({
-      name: title,
-      description,
-      url: projectLink,
-      icon: `https://sznm.dev${icon}`,
-    })
-  );
-
-  return new Response(JSON.stringify(projectList), {
-    status: 200,
-    headers: {
-      ...defaultHeader,
-      'access-control-allow-origin': '*',
-      'cache-control': 's-maxage=600, stale-while-revalidate=2678400',
-    },
-  });
 };
 
 export default projects;
