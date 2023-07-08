25c951732a1e24adbbcc90df76198f257ebbe5ac
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-07-08 11:35:37 +0700

    fix: export missing generateMetadata for snippet detail page

--- src/app/(core)/snippets/[id]/page.ts
+++ src/app/(core)/snippets/[id]/page.ts
@@ -2,4 +2,6 @@ import SnippetDetail from '~/lib/pages/snippets/detail';
 
 export { generateStaticParams } from '~/lib/pages/snippets/detail/loader';
 
+export { generateMetadata } from '~/lib/pages/snippets/detail/meta';
+
 export default SnippetDetail;
