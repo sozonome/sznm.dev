745fff978cbb7305cc36a7ed83aa74a8afd0a599
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-08 23:56:26 +0700

    chore: setup env validation using t3-oss/env (#214)

--- src/lib/constants/env.ts
+++ src/lib/constants/env.ts
@@ -1,1 +1,24 @@
-export const SHORTENER_HOST_NAME = process.env.SHORTENER_HOST_NAME ?? '';
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+const requiredString = z.string().min(1);
+const requiredUrl = z.string().url();
+
+export const env = createEnv({
+  server: {
+    SPOTIFY_CLIENT_ID: requiredString,
+    SPOTIFY_CLIENT_SECRET: requiredString,
+    SPOTIFY_REFRESH_TOKEN: requiredString,
+    SHORTENER_HOST_NAME: requiredString,
+    PORTFOLIO_ZONE_URL: requiredUrl,
+    PORTFOLIO_BUILD_ID: requiredString,
+  },
+  client: {
+    NEXT_PUBLIC_UMAMI_WEBSITE_ID: requiredString,
+    NEXT_PUBLIC_UMAMI_SRC: requiredString,
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+    NEXT_PUBLIC_UMAMI_SRC: process.env.NEXT_PUBLIC_UMAMI_SRC,
+  },
+});
