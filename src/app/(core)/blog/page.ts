3a7d24bc29dcadf0b3e663b832da053745c57562
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-10-16 21:17:07 +0700

    chore: update packages to latest, apply force-dynamic for view count pages

--- src/app/(core)/blog/page.ts
+++ src/app/(core)/blog/page.ts
@@ -2,4 +2,5 @@ import BlogPostList from '~/lib/pages/blog/list';
 
 export { metadata } from '~/lib/pages/blog/list/meta';
 
+export const dynamic = 'force-dynamic';
 export default BlogPostList;
--- src/app/(core)/notes/page.ts
+++ src/app/(core)/notes/page.ts
@@ -2,4 +2,5 @@ import NoteList from '~/lib/pages/note/list';
 
 export { metadata } from '~/lib/pages/note/list/meta';
 
+export const dynamic = 'force-dynamic';
 export default NoteList;
