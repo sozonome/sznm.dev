7b49e300c3d4f94776eca5bedb323c16d874131c
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-02-20 00:12:27 +0700

    chore: update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ["text", "html"],
-      include: ["src/lib/utils/**"],
+      reporter: ['text', 'html'],
+      include: ['src/lib/utils/**'],
       // include: ["src/lib/{utils,components}/**"] when more than 1 folder
       // exclude: [".next/**"],
       all: true,
